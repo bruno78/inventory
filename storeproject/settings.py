33e74d1257b2cf743e4cb474bd40f036b5ec9191
Bruno G. Tavares <devbruno78@gmail.com>
2017-12-21 00:28:45 -0500

    Add views

--- storeproject/settings.py
+++ storeproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'storeproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['storeproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
