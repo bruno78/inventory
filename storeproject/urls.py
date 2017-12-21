6e5720e1b20ab397e0df3bedeb90634beafe401e
Bruno G. Tavares <devbruno78@gmail.com>
2017-12-20 22:42:04 -0500

    Add model Item

--- storeproject/urls.py
+++ storeproject/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'firstdjango.views.home', name='home'),
+    # url(r'^$', 'storeproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
